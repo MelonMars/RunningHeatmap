634e59fc5b3b9945a06737cefdc3c11ee2c89b1c
carter <carterpfaff@gmail.com>
2024-07-23 12:52:26 -0400

    Takes super dang long and idk if it even works. Got a working version earlier but that version only got the part that overlaps between all of them, not just parts overlapping between 2.

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import gpxpy.gpx
 import folium
 from geopy.distance import great_circle
 import os
-from itertools import product
-from rtree import index
 
-
-def is_near(p1, p2, threshold=0.001):
-    return great_circle((p1[0], p1[1]), (p2[0], p2[1])) < threshold
+def is_close(pt1, pt2, tolerance=1):
+    return great_circle(pt1, pt2).meters <= tolerance
 
 
 points = []
@@ -16,24 +13,64 @@ for i, file in enumerate(os.listdir("GPX")):
     fPoints = []
     with open("GPX/"+file, "r") as gpxFile:
         gpx = gpxpy.parse(gpxFile)
-        for track in gpx.tracks:
-            for segment in track.segments:
-                for point in segment.points:
-                    fPoints.append([point.latitude, point.longitude])
-    points.append(fPoints)
-
-overlaps = []
-all_points = [p for sublist in points for p in sublist]
-buffer_size = 0.00001
-idx = index.Index()
-for i, point in enumerate(all_points):
-    idx.insert(i, (point[1] - buffer_size, point[0] - buffer_size, point[1] + buffer_size, point[0] + buffer_size))
-for i, point in enumerate(all_points):
-        possible_matches = list(idx.intersection((point[1] - buffer_size, point[0] - buffer_size, point[1] + buffer_size, point[0] + buffer_size)))
-        if any(is_near(point, all_points[match]) for match in possible_matches):
-            overlaps.append(point)
+        points.append([[point.latitude, point.longitude] for track in gpx.tracks for segment in track.segments for point in segment.points])
 
 
-m = folium.Map(location=(overlaps[0][0], overlaps[0][1]))
-folium.PolyLine(overlaps).add_to(m)
-m.save("index.html")
+def contiguousOverlaps(points, tolerance=50):
+    print("Contiguous Overlaps")
+    cont_segs = []
+    overIs = segmentIndex(points, tolerance)
+    if not overIs:
+        return cont_segs
+
+    curr_seg = [points[overIs[0]]]
+    for i in range(1, len(overIs)):
+        if overIs[i] == overIs[i - 1] + 1:
+            curr_seg.append(points[overIs[i]])
+        else:
+            cont_segs.append(curr_seg)
+            curr_seg = [points[overIs[i]]]
+    cont_segs.append(curr_seg)
+
+    return cont_segs
+
+
+def segmentIndex(points, tolerance=1):
+    indices = []
+    for i, pt in enumerate(points):
+        if i == 0:
+            continue
+        if is_close(pt, points[i-1], tolerance):
+            indices.append(i)
+    return indices
+
+tolerance = 1
+flat_points = [point for sublist in points for point in sublist]
+all_overlaps = []
+for run in points:
+    overlaps = []
+    for i, pt in enumerate(points[0]):
+        overlaps = []
+        for other_run in points:
+            if run == other_run:
+                continue
+            for pt in run:
+                if any(is_close(pt, other_pt, tolerance) for other_pt in other_run):
+                    overlaps.append(pt)
+
+        cps = contiguousOverlaps(overlaps, tolerance)
+        all_overlaps.append(cps)
+
+overlapCnts = {}
+for contiguous_parts in all_overlaps:
+    for part in contiguous_parts:
+        partT = tuple(part)
+        if partT in overlapCnts:
+            overlapCnts[partT] += 1
+        else:
+            overlapCnts[partT] = 1
+
+sorted_overlaps = sorted(overlapCnts.items(), key=lambda x: x[1], reverse=True)
+
+for segment, count in sorted_overlaps:
+    print(f"Segment: {segment}, Count: {count}")
